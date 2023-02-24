e5f93e590328cd9417a88a5fa49f511c491d5920
mrcreel <michaelrcreel@gmail.com>
2023-02-24 15:30:35 -0600

    Fixed <html lang=> typo

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document () {
   return (
-    <Html html='eng'>
+    <Html html='en'>
       <Head />
       <body>
         <Main />
